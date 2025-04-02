bd138fc363f40a339c06e7c1c099065892749ce8
fisayo-dev <olufisayobadina@gmail.com>
2025-04-02 13:52:18 +0100

    style: overall ui imrovement & look

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -1,25 +1,32 @@
-import Image from "next/image"
+import Image from 'next/image';
 
 const Landing = () => {
   return (
-      <div>
-          
-          <div className="app-container py-4">
-              <div className="flex min-h-[70vh] items-center justify-between">
-                  <div className="flex flex-col">
-                      <h2 className="text-[6rem]">I am inevitable</h2>
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem omnis eveniet quos tempora deleniti nemo, reprehenderit officia rem odio error.</p>
-                  </div>
-                  <Image
-                      width={50}
-                      height={50}
-                      src='/next.svg'
-                      alt="THansomimahe"
-                  />
-              </div>
-          </div>
+    <div className="app-container py-12">
+      <div className="flex flex-col md:flex-row items-center justify-between min-h-[70vh] gap-8">
+        {/* Left Content */}
+        <div className="text-center md:text-left max-w-2xl">
+          <h2 className="text-5xl md:text-7xl font-bold leading-tight">
+            I am inevitable
+          </h2>
+          <p className="mt-4 text-lg w-full md:w-4/5 md:text-xl text-muted-foreground">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem omnis eveniet quos tempora deleniti nemo, reprehenderit officia rem odio error.
+          </p>
+        </div>
+        
+        {/* Right Image */}
+        <div className="flex justify-center md:justify-end">
+          <Image 
+            width={400} 
+            height={400} 
+            src="/globe.svg" 
+            alt="Thanos image" 
+            className="w-64 md:w-96 object-contain"
+          />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
