fbf8728083996bbf09bec610a7fba1c76426799d
fisayo-dev <olufisayobadina@gmail.com>
2025-04-02 15:27:59 +0100

    chore: init powers component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Header from '@/components/Header';
 import Landing from '@/components/Landing';
+import Powers from '@/components/Powers';
 
 export default function Home() {
   return (
     <>
       <Header />
       <Landing />
+      <Powers />
     </>
   );
 }
