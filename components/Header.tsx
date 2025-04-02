f02970ee31b39d3a561d850c8e09329b0a9b587d
fisayo-dev <olufisayobadina@gmail.com>
2025-04-02 13:28:58 +0100

    style: custom font, them colors

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,11 @@
 const Header = () => {
-  return <div className="app-container py-4">
-    <div className="flex justify-between items-center">
-      <h2 className="text-5xl header-text">Thanos</h2>
+  return (
+    <div className="app-container py-4">
+      <div className="flex justify-between items-center">
+        <h2 className="text-5xl header-text ">Thanos</h2>
+      </div>
     </div>
-    </div>;
+  );
 };
 
 export default Header;
