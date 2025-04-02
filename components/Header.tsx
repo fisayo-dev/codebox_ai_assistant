aaf60db1de9a0b43f122af2c2cbd2f70d746a76e
fisayo-dev <olufisayobadina@gmail.com>
2025-04-02 13:36:35 +0100

    style: add translucent feature to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,29 @@
 const Header = () => {
   return (
-    <div className="app-container py-4">
-      <div className="flex justify-between items-center">
-        <h2 className="text-5xl header-text">Thanos</h2>
-        <ul className="other-text flex items-center space-x-4">
-          <li className="hover:text-gray-300 text-xl cursor-pointer">Home</li>
-          <li className="hover:text-gray-300 text-xl cursor-pointer">Powers</li>
-          <li className="hover:text-gray-300 text-xl cursor-pointer">Origin</li>
-          <li className="hover:text-gray-300 text-xl cursor-pointer">Mission</li>
-          <li className="hover:text-gray-300 text-xl cursor-pointer">Team</li>
-          <li className="hover:text-gray-300 text-xl cursor-pointer">Enemies</li>
-          <li className="hover:text-gray-300 text-xl cursor-pointer">Newsletter</li>
-        </ul>
+    <div className="bg-transparent backdrop-blur-">
+      <div className="app-container py-6">
+        <div className="flex justify-between items-center">
+          <h2 className="text-5xl header-text">Thanos</h2>
+          <ul className="other-text flex items-center space-x-4">
+            <li className="hover:text-gray-300 text-xl cursor-pointer">Home</li>
+            <li className="hover:text-gray-300 text-xl cursor-pointer">
+              Powers
+            </li>
+            <li className="hover:text-gray-300 text-xl cursor-pointer">
+              Origin
+            </li>
+            <li className="hover:text-gray-300 text-xl cursor-pointer">
+              Mission
+            </li>
+            <li className="hover:text-gray-300 text-xl cursor-pointer">Team</li>
+            <li className="hover:text-gray-300 text-xl cursor-pointer">
+              Enemies
+            </li>
+            <li className="hover:text-gray-300 text-xl cursor-pointer">
+              Newsletter
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
